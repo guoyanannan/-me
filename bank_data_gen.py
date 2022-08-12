366b9a218a8be3f3be3d70de007dca9b4d856e62
“GYN” <329432072@qq.com>
2022-08-12 11:18:16 +0800

    ‘v1.1.1’

--- bank_data_gen.py
+++ bank_data_gen.py
@@ -26,20 +26,26 @@ def encrypt_oracle():
                              'u','v','w','x','y','z']
                        },
                 '冷轧':{
-                      '英文':['SuiBian', 'ZheBian', 'QieKouBuLiang', 'KuanDuYiChang', 'JiaShang', 'JuBuWeiQieBian', 'CanJian', 'HouDuYiChang', 'BianSun', 'GuaBian', 'GongQi', 'QiaoQu',
+                      '英文':[
+                            'AnXian', 'BaiXianTiao', 'CaShangXingDian', 'ChongZiYin', 'CuoBianYin', 'HeiDian', 'HengXiangYaGunYin', 'LuZhaYin', 'SeCha', 'ShuiYin', 'ShuiYinBaiDian', 'TuoPiXian',
+                            'YaShangDian', 'XiHuaShang', 'XiaoSuiLang','ZhuangShangYin','ZongXiangZheHenYin','GunShangYin','HengXiangGunYin','BeiJing',
+                            'SuiBian', 'ZheBian', 'QieKouBuLiang', 'KuanDuYiChang', 'JiaShang', 'JuBuWeiQieBian', 'CanJian', 'HouDuYiChang', 'BianSun', 'GuaBian', 'GongQi', 'QiaoQu',
                             'ZhongLang', 'BianLang', 'QiGu', 'LianDaoWan', 'ZhengMianLangZhuang', 'JiaoChaLang','HuXing', 'YingJuan', 'JiaZha', 'KongDong', 'BiaoMianFenCeng', 'BianSiYaRu',
                             'ReZhuaShang', 'JiaCeng', 'DianJiShang', 'YangHuaPiHen', 'GunYin', 'ZangWu', 'BiaoMianLieWen', 'YiWuYaRu', 'HuaShang', 'ZaoShang', 'CaShang', '2BXiaoBaiDian',
                             'YangCan', 'TuoPi', 'ReLieGunWen', 'ZheDie', 'BiaoMianCuCao', 'XiuBan', 'QiKongTuoPi', 'ShiKongDianShi', 'BanDian', 'SuanYin', 'TuiHuoGuoDu', 'TuiHuoBuZu',
-                            'QianSuan', 'GuoSuan', 'ZhenHen', 'JiaHen', 'GangJuanZheHen', 'TingJiHen','XiuPiYaRu', 'NianJiao', 'YouJiao', 'ShuiXiBuLiang', 'ShuiYin', 'ShuaGunYin',
+                            'QianSuan', 'GuoSuan', 'ZhenHen', 'JiaHen', 'ZheHen', 'TingJiHen','XiuPiYaRu', 'NianJiao', 'YouJiao', 'ShuiXiBuLiang', 'ShuiYin', 'ShuaGunYin',
                             'JiaoZhiJiCanLiu', 'BaHen', 'FuZuoWuYinHen', 'XiuPiBaHen', 'BanMianHei', 'YaHen', 'BaiBan', 'TingJiJuan', 'LianZhuBaoPian', 'YouBan', 'SeZeAn','HeiXian',
                             'ErCiTuiHuoJuan', 'JianSuJuan', 'SuanNi', 'AoKeng', 'TingCheBan', 'QiPi', 'XianFeng', 'GuaShang', 'GuaHen', 'TiaoZhuangTuoPi', 'PenHaoCanLiu', 'LieWenGunHen',
                             'FuShiDian', 'SuanXiBuZu', 'BiaoMian2D', 'YanMoHen', 'SeZeBuJun','ZhiYinHen', 'JuanQuHen', 'YaYin', 'SuanXiBuJun', 'GuangZeTiaoWen', 'PaiGuWen', 'ZhaRuXiuPi2B',
                             'TaXing', 'BaGuaJuan', 'CeYuanBoLangJuan', 'SongSanGangJuan', 'NeiQuanTaXian', 'TuoYuanGangJuan', 'DanZhongBuZu', 'SanLeiTuoPi', 'HanFeng','YiLeiTuoPi', 'ErLeiTuoPi',
                             'SiLeiTuoPi', 'HanKong', 'HanFengBuDing', 'ZhaJiHen', 'XiuDian', 'BianBuYaXiu', 'BaoGuangSeCha', 'ReYaGuaShang', 'HeiDai', 'ReGunYin', 'ZaoShangFenSan',
                             'JianDuanReYaGuaShang', 'XianXingJiaZa', 'CengJianCaShang','ErLeiReYaGuaShang', 'YanZhongYangHuaPiTuoLuoHen'],
-                      '中文':['碎边', '折边', '切口不良', '宽度异常', '夹伤', '局部未切边', '残剪', '厚度异常', '边损', '刮边', '拱起', '翘曲', '中浪', '边浪', '起鼓', '镰刀弯', '整面浪状', '交叉浪',
+                      '中文':[
+                            '暗线', '白线条', '擦伤星点', '虫子印', '挫边印', '黑点', '横向压辊印', '炉渣印', '色差', '水印', '水印白点', '脱皮线','压伤点', '细划伤', '小碎浪','撞伤印','纵向折痕印',
+                            '辊伤印','横向辊印','背景',
+                            '碎边', '折边', '切口不良', '宽度异常', '夹伤', '局部未切边', '残剪', '厚度异常', '边损', '刮边', '拱起', '翘曲', '中浪', '边浪', '起鼓', '镰刀弯', '整面浪状', '交叉浪',
                             '弧形', '硬卷', '夹渣', '孔洞', '表面分层', '边丝压入', '热抓伤', '夹层', '电击伤', '氧化皮痕', '辊印', '脏污', '表面裂纹', '异物压入', '划伤', '凿伤', '擦伤', '2B小白点',
-                            '氧残', '脱皮', '热裂辊纹', '折叠', '表面粗糙', '锈斑', '气孔脱皮', '蚀孔点蚀', '斑点', '酸印', '退火过度', '退火不足', '欠酸', '过酸', '振痕', '夹痕', '钢卷折痕', '停机痕',
+                            '氧残', '脱皮', '热裂辊纹', '折叠', '表面粗糙', '锈斑', '气孔脱皮', '蚀孔点蚀', '斑点', '酸印', '退火过度', '退火不足', '欠酸', '过酸', '振痕', '夹痕', '折痕', '停机痕',
                             '锈皮压入', '黏胶', '油胶', '水洗不良', '水印', '刷辊印', '胶脂剂残留', '疤痕', '附作物印痕', '锈皮疤痕', '板面黑', '压痕', '白斑', '停机卷', '连铸剥片', '油班', '色泽暗',
                             '黑线', '二次退火卷', '减速卷', '酸泥', '凹坑', '停车斑', '起皮', '线缝', '刮伤', '刮痕', '条状脱皮', '喷号残留', '裂纹辊痕', '腐蚀点', '酸洗不足', '表面2D', '研磨痕', '色泽不均',
                             '纸印痕', '卷曲痕', '压印', '酸洗不均', '光泽条纹', '排骨纹', '扎入锈皮2B', '塔形', '八卦卷', '侧缘波浪卷', '松散钢卷', '内圈塌陷', '椭圆钢卷', '单重不足', '三类脱皮', '焊缝',
@@ -48,15 +54,15 @@ def encrypt_oracle():
 
                        },
                 '热轧':{
-                       '英文':['DaiFenLei', 'JingZhaGunYin', 'DaiTouGunYin', 'ZhaLan', 'KongDong', 'ZhaRuWaiWu','BaoPian', 'JieBa', 'XianZhuangJiaZa', 'ZhuPiHuaShang', 'ZongXiangLieWen',
-                             'GuaHen','LiangHuaShang', 'XiaFeng', 'YiCiXiuPi', 'ErCiXiuPi', 'TieLinYaRu', 'YangHuaTiePi','ZhenHen', 'PianZhuangTieLin', 'BoXing', 'ShuiDi', 'ShuiWu',
-                             'ShuiYin','BaoGuangYinHen', 'TingZhiShuXian', 'BaiTieLin', 'BeiJingYi', 'BeiJingEr', 'BeiJingSan','BeiJingSi', 'QiPi', 'TouWeiBian', 'BianYuanPoLie',
-                             'BeiJingWu', 'BeiJingLiu','BianYuanMaoCi', 'GuoBaoGuang', 'XiuPiTuoLuo', 'CaBa', 'BeiJingQi', 'BeiJingBa','YiWuYaRu', 'TuBao', 'LaSiYaRu', 'ZongXiangHuaShang',
-                             'AoKeng', 'CaShang', 'HuaShang', 'TiaoZhuangChongPi'],
+                       '英文':['DaiFenLei', 'JingZhaGunYin', 'DaiTouGunYin', 'YaLan', 'KongDong', 'ZhaRuWaiWu','BaoPian', 'JieBa', 'XianZhuangJiaZa', 'ZhuPiHuaShang', 'ZongXiangLieWen', 'GuaHen','LiangHuaShang', 'XianFeng', 'YiCiXiuPi', 'ErCiXiuPi', 'TieLinYaRu', 'YangHuaTiePi',
+                             'ZhenHen','PianZhuangTieLin', 'BoXing', 'ShuiDi', 'ShuiWu', 'ShuiYin','BaoGuangYinHen', 'TingJiWenLi', 'BaiTieLin', 'ReZhaBianBuZheDie', 'ReZhaJuChiBian', 'LianGangHuaShang','LianGangJiaZa', 'LianGangXiWeiJiaZa', 'LianGangFenCeng', 'LianGangHeiDai', 'LianGangXianXingJiaZaWu',
+                             'MXingLieWen','ReZhaBanBianHuaHen','ZhiGangXianXingJiaZa', 'ZhiGangYanMoHen', 'ReYaMoCaHen', 'LianGangBa', 'GongZuoGunMaDian','YiWuYaRu', 'TuBao', 'LaSiYaRu', 'AoKeng', 'CaShang', 'HuaShang', 'TiaoZhuangChongPi','LianGangBianBuKaiLie','ReZhaRCLHen',
+                             'ReZhaGunYin','ReZhaJiaSongGunYin','ReZhaYangHuaPiBoLi','ReZhaZhanGang',],
 
                        '中文':['待分类', '精轧辊印', '带头辊印', '轧烂', '孔洞', '轧入外物','剥片', '结疤', '线状夹杂', '铸坯划伤', '纵向裂纹', '刮痕','亮划伤', '狭缝', '一次锈皮', '二次锈皮', '铁鳞压入', '氧化铁皮',
-                             '振痕','片状铁鳞', '波形', '水滴', '水雾', '水印','曝光印痕', '停止竖线', '白铁鳞', '背景一', '背景二', '背景三','背景四', '起皮', '头尾边', '边缘破裂', '背景五', '背景六','边缘毛刺',
-                             '过曝光', '锈皮脱落', '擦疤', '背景七', '背景八','异物压入', '凸包', '拉丝压入', '纵向划伤', '凹坑', '擦伤', '划伤', '条状重皮']
+                             '振痕','片状铁鳞', '波形', '水滴', '水雾', '水印','曝光印痕', '停机纹理', '白铁鳞', '热轧边部折叠', '热轧锯齿边', '炼钢划伤','炼钢夹杂', '炼钢细微夹杂', '炼钢分层', '炼钢黑带', '炼钢线性夹杂物', 
+                             'M型裂纹','热轧板边划痕','制钢线性夹杂', '制钢研磨痕', '热轧摩擦痕', '炼钢疤', '工作辊麻点','异物压入', '凸包', '拉丝压入', '凹坑', '擦伤', '划伤', '条状重皮','炼钢边部开裂','热轧RCL痕',
+                             '热轧辊印','热轧夹送辊印','热轧氧化皮剥离','热轧粘钢',]
                        },
                 '板材':{
                        '英文':['DaiFenLei', 'ZongXiangLieWen', 'HengXiangLieWen', 'BianLie', 'ShuiYin', 'GunYin',
@@ -126,20 +132,20 @@ def decrypt_oralce():
     decrypted_text = str(aes.decrypt(base64_decrypted),encoding='utf-8') # 执行解密密并转码返回str
     decrypted_text = base64.b64decode(decrypted_text.encode('utf-8')).decode('utf-8')
     # print(decrypted_text)
-    print(json.loads(decrypted_text))
+    #print(json.loads(decrypted_text))
     # print(type(json.loads(decrypted_text)))
-    print(json.loads(decrypted_text)['冷轧']['英文'],':\n',len(json.loads(decrypted_text)['冷轧']['英文']))
+    print('----------冷轧----------- \n',json.loads(decrypted_text)['冷轧']['英文'],':\n',len(json.loads(decrypted_text)['冷轧']['英文']))
     print(json.loads(decrypted_text)['冷轧']['中文'],':\n',len(json.loads(decrypted_text)['冷轧']['中文']))
-    print(json.loads(decrypted_text)['热轧']['英文'],':\n',len(json.loads(decrypted_text)['热轧']['英文']))
+    print('----------热轧----------- \n',json.loads(decrypted_text)['热轧']['英文'],':\n',len(json.loads(decrypted_text)['热轧']['英文']))
     print(json.loads(decrypted_text)['热轧']['中文'],':\n',len(json.loads(decrypted_text)['热轧']['中文']))
-    print(json.loads(decrypted_text)['板材']['英文'],':\n',len(json.loads(decrypted_text)['板材']['英文']))
+    print('----------板材----------- \n',json.loads(decrypted_text)['板材']['英文'],':\n',len(json.loads(decrypted_text)['板材']['英文']))
     print(json.loads(decrypted_text)['板材']['中文'],':\n',len(json.loads(decrypted_text)['板材']['中文']))
-    print(json.loads(decrypted_text)['棒材']['英文'],':\n',len(json.loads(decrypted_text)['棒材']['英文']))
+    print('----------棒材----------- \n',json.loads(decrypted_text)['棒材']['英文'],':\n',len(json.loads(decrypted_text)['棒材']['英文']))
     print(json.loads(decrypted_text)['棒材']['中文'],':\n',len(json.loads(decrypted_text)['棒材']['中文']))
-    print(json.loads(decrypted_text)['铸坯']['英文'],':\n',len(json.loads(decrypted_text)['铸坯']['英文']))
+    print('----------铸坯----------- \n',json.loads(decrypted_text)['铸坯']['英文'],':\n',len(json.loads(decrypted_text)['铸坯']['英文']))
     print(json.loads(decrypted_text)['铸坯']['中文'],':\n',len(json.loads(decrypted_text)['铸坯']['中文']))
-    print(json.loads(decrypted_text)['字符']['英文'],':\n',len(json.loads(decrypted_text)['字符']['英文']))
-    print(json.loads(decrypted_text)['字符']['中文'],':\n',len(json.loads(decrypted_text)['字符']['中文']),type(json.loads(decrypted_text)['字符']['中文']))
+    print('----------字符----------- \n',json.loads(decrypted_text)['字符']['英文'],':\n',len(json.loads(decrypted_text)['字符']['英文']))
+    print(json.loads(decrypted_text)['字符']['中文'],':\n',len(json.loads(decrypted_text)['字符']['中文']))
 
 
 
